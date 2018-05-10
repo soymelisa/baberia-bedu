6f40ddf132f9098f6567daf8275cabdf81379f52
zerookrash <mario.villelalarrazagmail.com>
2018-05-09 20:01:26 -0500

    Login

---
+++ src/components/SignUp.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+  .SignUp
+    h3 Crea una nueva cuenta!
+    input(type="email", v-model="email", placeholder="Email")
+    br
+    input(type="password", v-model="password", placeholder="Password")
+    br
+    button(v-on:click='signUp') SignUp
+    p
+      | Regresa a 
+      router-link(to='/login') Login
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'signUp',
+  data: function () {
+    return {}
+  },
+  methods: {
+    signUp: function () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          this.$router.replace('hello')
+        },
+        (err) => {
+          alert('Upss!' + err.message)
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  .SignUp
+    magin-top: 40px
+    font-size: 30px
+    h3
+      font-size: 40px
+  input
+    margin: 10px 0
+    width: 50%
+    padding: 15px
+    border-radius: 15px
+  
+  button
+    magin-top: 20px
+    width: 30%
+    cursor: pointer
+
+  p
+    magin-top: 80px
+    font-size: 15px
+
+  p a 
+    text-decoration: underline
+    cursor: pointer
+</style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+let app
+let config = {
+  apiKey: 'AIzaSyCm_GiZT5kC-VBxFpPRB2F-X1uZemOHHRY',
+  authDomain: 'barberia-6dfab.firebaseapp.com',
+  databaseURL: 'https://barberia-6dfab.firebaseio.com',
+  projectId: 'barberia-6dfab',
+  storageBucket: 'barberia-6dfab.appspot.com',
+  messagingSenderId: '50399763158'
+}
+firebase.initializeApp(config)
+firebase.auth().onAuthStateChanged(function (user) {
+  if (!app) {
+    app = new Vue({
+      el: '#app',
+      router,
+      template: '<App/>',
+      components: { App }
+    })
+  }
 })
+
