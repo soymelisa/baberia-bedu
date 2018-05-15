b5a39a5933245563159306825b87eb5f1d10a0cf
zerookrash <mario.villelalarrazagmail.com>
2018-05-15 18:22:57 -0500

    Formulario de Perfil del Usuario

--- src/components/ProfileComponents/Login.vue
+++ src/components/ProfileComponents/Login.vue
@@ -47,10 +47,28 @@
         )
       },
       loginFB: function () {
-        FB.getLoginStatus(function(response) { 
-          console.log(response)
-          statusChangeCallback(response)
+        var provider = new firebase.auth.FacebookAuthProvider()
+        var _this = this
+        provider.setCustomParameters({
+          'display': 'popup'
         })
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+            _this.$router.replace('perfil')
+        }).catch(function(error) {
+          // Handle Errors here.
+          console.log(error)
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
       }
     }
   }
--- src/components/ProfileComponents/Perfil.vue
+++ src/components/ProfileComponents/Perfil.vue
@@ -1,7 +1,33 @@
 <template lang="pug">
-  h1 Perfil de usuario.
-    br
-    button(v-on:click='logout') Logout
+  .container
+    hr
+    b-nav(fill='')
+      b-nav-item Home
+      b-nav-item Citas
+      b-nav-item Perfil
+      b-nav-item(v-on:click='logout') Logout
+    hr
+  
+    form#perfil(@submit='checkForm', method='post')
+      p(v-if='errors.length')
+        b Por favor complete los siguientes campos:
+        ul
+          li(v-for='error in errors') {{ error }}
+      p
+        //- label(for='nombre') Nombre
+        input#nombre(type='text', name='nombre', v-model='nombre', placeholder='Nombre')
+      p
+        //- label(for='apellidos') Apellidos
+        input#apellidos(type='text', name='apellidos', v-model='apellidos', placeholder='Apellidos')
+      p
+        //- label(for='edad') Edad
+        input#age(type='number', name='edad', v-model='edad', min='0', placeholder='Edad')
+      p
+        input#correo(type='email', name='correo', v-model='correo', placeholder='Correo Eléctronico')
+      p
+        input#telefono(type='text', name='telefono', v-model='telefono', placeholder='Teléfono')
+      p
+        input.boton(type='submit', value='Submit')
 </template>
 
 <script>
@@ -9,10 +35,25 @@ import firebase from 'firebase'
 
 export default {
   name: 'perfil',
-  data () {
-    return {}
+  data: function () {
+    return {
+      errors: [],
+      nombre: null,
+      apellidos: null,
+      edad: null,
+      correo: null,
+      telefono: null
+    }
   },
   methods: {
+    checkForm: function (e) {
+      if (this.name && this.age) return true
+      this.errors = []
+      if (!this.nombre) this.errors.push('Nombre.')
+      if (!this.correo) this.errors.push('Contacto.')
+      if (!this.telefono) this.errors.push('Teléfono.')
+      e.preventDefault()
+    },
     logout: function () {
       firebase.auth().signOut().then(() => {
         this.$router.replace('login')
@@ -23,22 +64,35 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="sass" scoped>
+h1, h2  
+  font-weight: normal
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+ul 
+  list-style-type: none
+  padding: 0
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+li 
+  display: inline-block
+  margin: 0 10px
 
-a {
-  color: #42b983;
-}
+a 
+  color: #42b983
+
+input
+    margin: 10px 0
+    width: 50%
+    padding: 15px
+    border-radius: 15px
+
+.navBar
+  border-radius: 15px
+  background-color: rgba(104, 178, 255, 0.4)
+  .item
+    color: black
+
+.boton
+    magin-top: 20px
+    width: 30%
+    cursor: pointer
 </style>
